8cce3b0ea50b44a6ce3a705fd43a5d7e306e3493
stepin104493 <69049323+stepin104493@users.noreply.github.com>
2020-08-14 12:06:58 +0530

    Add files via upload

---
+++ assignment/function/header/prime.h
@@ -0,0 +1,13 @@
+#ifndef PRIME_H_INCLUDED
+#define PRIME_H_INCLUDED
+
+/**
+* Checks if the integer is prime or not
+* @param[in] number for which  has to be checked
+* @return Prime or not
+* @note Returns 0 for not prime
+*/
+int primenum(int number);
+
+
+#endif // PRIME_H_INCLUDED
