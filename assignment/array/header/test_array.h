4b444d80aa29c41b0b61945264f19983706744dc
stepin104493 <69049323+stepin104493@users.noreply.github.com>
2020-08-14 12:36:13 +0530

    Add files via upload

---
+++ assignment/array/header/test_array.h
@@ -0,0 +1,6 @@
+#ifndef TEST_ARRAY_H_INCLUDED
+#define TEST_ARRAY_H_INCLUDED
+
+int test_main(void);
+
+#endif // TEST_ARRAY_H_INCLUDED
---
+++ assignment/array/src/array_sort.c
@@ -0,0 +1,11 @@
+#include "array_sort.h"
+int sort(int n)
+{
+    if(n>1)
+{
+
+    return 0;
+}
+    else
+        return -1;
+}
